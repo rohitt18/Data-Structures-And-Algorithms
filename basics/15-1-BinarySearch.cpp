78e0d44ea1fa1e57ca9e52c3aa0522e8619d7cac
Rohit Gupta <rohitgupta9769@gmail.com>
2022-05-24 09:31:21 +0530

    binary search

---
+++ basics/15-1-BinarySearch.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+// increasing or decreasing array
+
+int binarySearch(int arr[], int size, int key)
+{
+    int start = 0;
+    int end = size - 1;
+
+    int mid = start + (end - start) / 2;
+
+    while (start <= end) // tab tak game chalne do
+    {
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        if (key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else //  key < arr[mid]
+        {
+            end = mid - 1;
+        }
+
+        // start & end update ho gaye honge isliye mid bhi update karna padega
+        mid = start + (end - start) / 2;
+    }
+    return -1;
+}
+
+int main()
+{
+    int even[6] = {2, 4, 6, 8, 12, 18};
+    int odd[5] = {3, 8, 11, 14, 16};
+
+    cout << "Index of 6 is " << binarySearch(even, 6, 6) << endl;
+    cout << "Index of 14 is " << binarySearch(odd, 5, 14) << endl;
+}
