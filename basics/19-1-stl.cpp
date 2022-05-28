f8a7c08cfd50a47b7362dca14fe7922d55eb524d
Rohit Gupta <rohitgupta9769@gmail.com>
2022-05-29 04:59:48 +0530

    stl

---
+++ basics/19-1-stl.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    C++ STL => 1.Algorithms
+            => 2.Containers
+            
+    Algorithms => 1.Binary Search
+               => 2.lower/Upper Bound  
+               => 3.min/Max
+               => 4.reverse/rotate
+               => 5.sort/swap etc
+
+    Containers => 1.Sequence Containers (Array,Vector,Deque,List,forward_list)
+               => 2.Container Adaptors (Stack,Queue,Priority Queue)
+               => 3.Associative Containers (Set,Map,MultiSet,MultiMap)
+               => 4.Unordered Associative (Unordered Set,Unordered Map,Unordered multiset,Unordered multimap)
+            
+}
---
+++ basics/19-3-vector.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// size = kitne element pade hai(kitne element iske vector ke andar pada hue hai)
+// capacity = kitne element ke liye space assign hua pada hai(kitni memory isko assign hui padi hai)
+
+int main()
+{
+
+    vector<int> v; // initializing a vector
+
+    vector<int> a(5,1); // initializing a vector a with size 5 and shuru mai initialize karde 1 ke sath
+    cout<<"print a"<<endl;
+    for(int i:a){
+        cout<<i<<" ";
+    }cout<<endl;
+
+    cout<<"Size-> " << v.size() << endl;
+
+    cout << "Capacity-> " << v.capacity() << endl;
+
+    v.push_back(1);
+    cout << "Capacity-> " << v.capacity() << endl;
+
+    v.push_back(2);
+    cout << "Capacity->  " << v.capacity() << endl;
+    cout<<"Size-> " << v.size() << endl;
+
+    v.push_back(3);
+    cout<<"Capacity-> " << v.capacity() << endl;
+    cout<<"Size-> " << v.size() << endl;
+
+    cout<<"Element at 2nd Index  " << v.at(2) << endl;
+
+    cout << "front " << v.front() << endl;
+    cout << "back " << v.back() << endl;
+
+    cout<<"before pop"<<endl;
+    for(int i:v){
+        cout<<i<<" ";
+    }cout<<endl;
+
+    v.pop_back();
+
+    cout << "after pop" << endl;
+    for (int i : v)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    cout<<"before clear size "<<v.size()<<endl;
+    v.clear();
+    cout<<"after clear size "<<v.size()<<endl;
+
+}
