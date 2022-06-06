f20a336e81a4f328fd7a12f47fcaf7d59e546b7c
Rohit Gupta <rohitgupta9769@gmail.com>
2022-06-06 22:55:32 +0530

    table print

---
+++ 40-questions-list/02-tablePrint.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cout<<"Enter the number : ";
+    cin>>n;
+
+    for(int i=1;i<=10;i++){
+        cout<<i*n<<endl;
+    }
+
+    return 0;
+}
