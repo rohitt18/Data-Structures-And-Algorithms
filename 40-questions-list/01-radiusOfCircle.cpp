63e26ff467cc6ffb5de6ea0c3e8b4ba4fb81027b
Rohit Gupta <rohitgupta9769@gmail.com>
2022-06-06 22:50:16 +0530

    area of circle

---
+++ 40-questions-list/01-radiusOfCircle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int r;
+    cout<<"Enter the radius of the circle ";
+    cin>>r;
+
+    cout<<"Radius of circle: " << r << endl;
+
+    cout<<"Area of circle: " << 3.14 * r * r << endl;
+
+    return 0;
+    
+}
