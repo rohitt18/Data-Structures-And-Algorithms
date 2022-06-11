f475a7d1842a9474c0b86bb8734e694da0766f74
Rohit Gupta <rohitgupta9769@gmail.com>
2022-06-11 11:32:03 +0530

    maximum circular subarray sum

--- array questions/max_circular_subarray_sum.cpp
+++ array questions/max_circular_subarray_sum.cpp
@@ -2,6 +2,22 @@
 #include <vector>
 using namespace std;
 
+
+int kadane(int arr[], int n){
+    int currSum = 0;
+    int maxi = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        currSum += arr[i];
+        maxi = max(maxi, currSum);
+        if (currSum < 0)
+        {
+            currSum = 0;
+        }
+    }
+    return maxi;
+}
+
 int main()
 {
 
@@ -14,35 +30,27 @@ int main()
         cin >> a[i];
     }
 
+    int wrapsum;       // x
+    int nonwrapsum;    //y
+
+    nonwrapsum = kadane(a,n);
+
     // calculating the sum of orginal array(totalSum)
     int totalSum = 0;
     for(int i=0;i<n;i++){
         totalSum = totalSum + a[i];
-    }
-
-    // inverting the a into a2 (matlab saare elements ko -ve kar diya)
-    vector<int> a2;
-
-    for(int i=0;i<n;i++){
-        a2.push_back(-1*a[i]);
+        a[i] = -a[i];
     }
 
     // applying kadane's algo on reversed sign wala array (a2)
     // that will give the sum of non contributing elements
-    int currSum = 0;
-    int maxi = INT_MIN;
-    for (int i = 0; i < n; i++)
-    {
-        currSum += a2[i];
-        maxi = max(maxi, currSum);
-        if (currSum < 0)
-        {
-            currSum = 0;
-        }
-    }
 
     // max subarray sum = total sum - sum of non-contributing elements
-    cout<<"Max subarray sum is "<< totalSum-(-maxi)<<endl;
+    wrapsum = totalSum + kadane(a, n);
+
+    int ans = max(wrapsum, nonwrapsum);
+
+    cout<<"Max subarray sum is "<< ans <<endl;
 
     return 0;
 }
