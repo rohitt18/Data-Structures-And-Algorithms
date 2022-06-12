2028e73ae2333c0de916657ab51635e679fc60f6
Rohit Gupta <rohitgupta9769@gmail.com>
2022-06-12 21:12:18 +0530

    maximum occuring character in a string

---
+++ string questions.cpp/maximum_occuring_character.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string s = "rohittgupptap";
+
+    int freq[26]={0};
+
+    for(int i=0;i<s.size();i++){
+        // lower case
+        int number;
+        if(s[i]>='a' && s[i]<='z'){
+             number = s[i] - 'a';
+        }
+        // upper case
+        else{
+            number = s[i] - 'A';
+        }
+        freq[number]++;
+    }
+
+    int ans = 0;
+    int maxi = -1;
+
+    for(int i=0;i<26;i++){
+        if(freq[i]>maxi){
+            maxi=freq[i];
+            ans = i;
+        }
+    }
+
+    char finalAns = ans + 'a';
+
+    cout << maxi << " " << finalAns << endl;
+
+    return 0;
+}
