cf2564564e99d9bd97e78491e34c5701c9dd298a
Rohit Gupta <rohitgupta9769@gmail.com>
2022-06-12 19:18:42 +0530

    functions of string

---
+++ revision/functions_of_strings.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;
+
+    string s1 = "fam";
+    string s2 = "ily"; 
+
+    // append.
+
+    // s1.append(s2);
+    // cout<<s1<<endl;
+    
+    // addition of 2 strings.
+
+    // str = s1 + s2;
+    // cout<<str<<endl;
+
+    string s = "786";
+    int ans = stoi(s);
+    cout<<ans+4<<endl;
+
+    return 0;
+}
