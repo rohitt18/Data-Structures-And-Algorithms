ecc258431bd9ead780be26f0b87a2abc014c10b1
Rohit Gupta <rohitgupta9769@gmail.com>
2022-06-12 18:06:30 +0530

    ways to declare a string

---
+++ revision/ways_toDeclare_aString.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // string str;
+    // string str1(5,'r');
+    // cout<<str1<<endl;
+    // // o/p --> rrrrr
+
+    string str;
+    getline(cin, str);
+    cout<<str<<endl;
+    // // i/p --> rishabh is a peaceful soul, o/p --> rishabh is a peaceful soul
+
+
+
+    return 0;
+}
