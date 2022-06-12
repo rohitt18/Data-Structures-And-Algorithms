a2c5a858213f643b0c7920c7b39abf04b037dc46
Rohit Gupta <rohitgupta9769@gmail.com>
2022-06-12 12:37:29 +0530

    character arrays intro

---
+++ revision/character_arrays_intro.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char arr[100] = "apple";
+
+    int i = 0;
+
+    while(arr[i] != '\0'){
+        cout<<arr[i++]<<endl;
+    }
+
+
+    return 0;
+}
