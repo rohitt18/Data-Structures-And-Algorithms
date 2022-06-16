eaa0ca54d4283aba94778b3380ec641faadb29c3
Rohit Gupta <rohitgupta9769@gmail.com>
2022-06-16 21:18:43 +0530

    pointers with functions

---
+++ basics/26-4-imp.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int getSum(int *arr, int n){
+
+    cout << endl << "Size : " << sizeof(arr) << endl;
+
+    int sum = 0;
+    for(int i=0;i<n;i++){
+        sum += arr[i];
+    }
+    return sum;
+}
+
+// int arr[] or int *arr ==> both of them are the same
+// arr[i] or i[arr] ==> both of them are the same
+
+int main()
+{
+    int arr[5] = {1,2,3,4,5};
+
+    cout << "Sum is " << getSum(arr,5);
+
+    return 0;
+}
