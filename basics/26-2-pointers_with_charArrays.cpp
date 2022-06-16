3546d61af79e37b3d3691241e631444a6e531a89
Rohit Gupta <rohitgupta9769@gmail.com>
2022-06-16 15:16:49 +0530

    pointers with character arrays

--- basics/26-2-pointers_with_charArrays.cpp
+++ basics/26-2-pointers_with_charArrays.cpp
@@ -7,13 +7,19 @@ int main()
     char ch[6] = "abcde";
 
     cout << arr << endl;
-
+    // attention here
     cout << ch << endl;
 
     char *c = &ch[0];
-    
+
     // prints entire string
     cout << c << endl;
+
+
+    char temp = 'z';
+    char *p = &temp;
+    
+    cout<<p<<endl;
 
     return 0;
 }
