a4bad5676170c24bd863c393a7159f6b062cfcd6
Rohit Gupta <rohitgupta9769@gmail.com>
2022-06-17 00:27:37 +0530

    double pointer

--- basics/27-1-double_pointers.cpp
+++ basics/27-1-double_pointers.cpp
@@ -1,16 +1,47 @@
 #include <iostream>
 using namespace std;
 
+void update(int **p2){
+    // p2 = p2 + 1;
+    // kuch change hoga? -- NO
+
+    // *p2 = *p2 + 1;
+    // kuch change hoga? -- YES
+
+    **p2 = **p2 + 1;
+    // kuch change hoga?
+}
+
 int main()
 {
     int i = 5;
     int* p = &i;
     int** p2 = &p;
 
+    /*
     cout << "printing p " << p << endl;
     // cout << "address of p " << &p << endl;
-    cout<<*p2<<endl;
+    cout << *p2 << endl;
 
+    cout << i << endl;
+    cout << *p << endl;
+    cout << **p2 << endl;
+
+    cout << &i << endl;
+    cout << p << endl;
+    cout << *p2 << endl;
+
+    cout << &p << endl;
+    cout << p2 << endl;
+    */
+
+    cout << "before " << i << endl;
+    cout << "before " << p << endl;
+    cout << "before " << p2 << endl;
+    update(p2);
+    cout << "after " << i << endl;
+    cout << "after " << p << endl;
+    cout << "after " << p2 << endl;
 
     return 0;
 }
