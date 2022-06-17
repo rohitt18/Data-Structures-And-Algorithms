87da558d4d3dddb0e9d9f6eac40062e86ecbcf21
Rohit Gupta <rohitgupta9769@gmail.com>
2022-06-17 21:23:08 +0530

    dynamic memory allocation in 2D arrays

--- basics/29-1-2d_dynamic_memory_allocation.cpp
+++ basics/29-1-2d_dynamic_memory_allocation.cpp
@@ -3,26 +3,30 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
+    int row;
+    cin >> row;
+
+    int col;
+    cin >> col;
 
     // creating a 2D array
-    int** arr = new int* [n];
-    for(int i=0;i<n;i++){
-        arr[i] = new int[n];
+    int** arr = new int* [row];
+    // har row ke liye kuch col daal rahe hai
+    for(int i=0;i<row;i++){
+        arr[i] = new int[row];
     }
     // creation DONE
 
-    // taking inputs
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    // taking inputs 
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
             cin>>arr[i][j];
         }
     }
 
     // printing the 2d array
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
+    for (int i = 0; i < row; i++){
+        for (int j = 0; j < col; j++){
             cout<<arr[i][j]<<" ";
         }cout<<endl;
     }
