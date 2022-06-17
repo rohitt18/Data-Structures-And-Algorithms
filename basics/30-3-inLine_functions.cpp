e5d6d043c44d51cdbfcfac08db3478980ed5e998
Rohit Gupta <rohitgupta9769@gmail.com>
2022-06-17 23:16:29 +0530

    inline functions

---
+++ basics/30-3-inLine_functions.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+// void func(int a,int b){
+//     a++;
+//     b++;
+//     cout<<a<<" "<<b<<endl;
+// }
+
+inline int getMax(int& a, int& b){
+    return (a > b) ? a : b;
+}
+
+int main()
+{
+    int a = 1 ,b = 2;
+
+    int ans=0;
+
+    // if(a>b){
+    //     ans = a;
+    // }else{
+    //     ans = b;
+    // }
+
+    ans = getMax(a,b);
+    cout << ans << endl;
+
+    a = a + 3;
+    b = b + 1;
+
+    ans = getMax(a, b);
+
+    cout<<ans<<endl;
+
+    // func(a,b);
+
+    return 0;
+}
