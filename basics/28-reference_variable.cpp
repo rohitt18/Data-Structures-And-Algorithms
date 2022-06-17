b5baa508d6c1ee76bc3f93ec4471f5ad32622003
Rohit Gupta <rohitgupta9769@gmail.com>
2022-06-17 18:13:28 +0530

    reference variables

--- basics/28-reference_variable.cpp
+++ basics/28-reference_variable.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
 
+void update1(int n){
+    n++;
+}
+
+void update2(int& n){
+    n++;
+}
+
 int main()
 {
+    /*
     int i = 5;
 
     // create a reference variable
@@ -14,6 +23,18 @@ int main()
     j++;
     cout << i << endl;
     cout << j << endl;
+    */
+
+    int n=5;
+    cout << "Before " << n << endl;
+    update1(n);
+    cout << "After " << n << endl;
+    
+    cout << "Before " << n << endl;
+    update2(n);
+    cout << "After " << n << endl;
+
+
 
     return 0;
 }
