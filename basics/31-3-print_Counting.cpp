56b63615ad184c93266cb929d720043927c861a9
Rohit Gupta <rohitgupta9769@gmail.com>
2022-06-20 22:06:18 +0530

    recursion print counting

--- basics/31-3-print_Counting.cpp
+++ basics/31-3-print_Counting.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
+// // TAIL RECURSION
+// void print(int n){
+
+//     // base case
+//     if(n==0){
+//         return;
+//     }
+
+//     // print (processing)
+//     cout << n <<" ";
+
+//     // recursive relation
+//     print(n-1);
+// }
+
+// HEAD RECURSION
 void print(int n){
 
     // base case
@@ -8,11 +24,12 @@ void print(int n){
         return;
     }
 
+    // recursive relation
+    print(n-1);
+
     // print (processing)
     cout << n <<" ";
 
-    // recursive relation
-    print(n-1);
 }
 
 int main()
