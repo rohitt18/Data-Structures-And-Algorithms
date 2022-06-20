b17ed1fc212f0c3de4fdc848a999b4dfc4d83e43
Rohit Gupta <rohitgupta9769@gmail.com>
2022-06-20 21:33:57 +0530

    recursion 1 factorial

--- basics/31-Recursion_factorial.cpp
+++ basics/31-Recursion_factorial.cpp
@@ -3,10 +3,14 @@ using namespace std;
 
 int factorial(int n){
 
+
     // base case
     if(n==0) return 1;
 
-    return n * factorial(n - 1);
+    int smallerProblem = factorial(n-1);
+    int biggerProblem = n * smallerProblem;
+
+    return biggerProblem;
 }   
 
 int main()
