293fef2b886c07b3190849ddd09f22f6f6a6b3be
Rohit Gupta <rohitgupta9769@gmail.com>
2022-07-04 16:30:27 +0530

    time & space complexity

--- basics/41-timeComplexity.cpp
+++ basics/41-timeComplexity.cpp
@@ -6,16 +6,16 @@
 
 // 2. Factorial 
 // T.C => O(n)
-// S.C => O(1)
+// S.C => O(n)
 
 // 3. Binary Search
 // T.C => O(logn)
-// S.C => O(1)
+// S.C => O(logn)
 
 // 4. Merge Sort
 // T.C => O(nlogn)
-// S.C => O(1)
+// S.C => O(n)
 
 // 5. Fibonacci 
 // T.C => O(2^n)
-// S.C => O(1)
+// S.C => O(n)
