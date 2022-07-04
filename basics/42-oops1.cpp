bf78c27422f0f80f2c55e77c12d5973a656d5213
Rohit Gupta <rohitgupta9769@gmail.com>
2022-07-04 19:07:35 +0530

    oops 1

---
+++ basics/42-oops1.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Hero {
+    
+    // properties
+    public:
+    int health;
+    char level;
+
+};
+
+int main()
+{   
+    // creation of object
+    Hero ramesh;
+
+    cout << "health is : " << ramesh.health << endl;
+    cout << "level is : " << ramesh.level << endl;
+
+    // cout<<"size : "<< sizeof(h1) << endl;
+
+    return 0;
+}
