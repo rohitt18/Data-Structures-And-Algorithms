ad720b4e537c991d74350dc8f2c01bfb169df19e
Rohit Gupta <rohitgupta9769@gmail.com>
2022-07-09 00:16:09 +0530

    dynamic allocation in oops

--- basics/42-oops1.cpp
+++ basics/42-oops1.cpp
@@ -33,21 +33,43 @@ class Hero {
 
 int main()
 {   
-    // creation of object
-    Hero ramesh;
+    // static allocation
+    Hero a;
+    // use setter for health bec it is private
+    a.setHealth(80);
+    a.setLevel('A');
+    cout << "level: " << a.level << endl;
+    cout << "health is " << a.getHealth() << endl;
 
-    cout<<"Ramesh health is " << ramesh.getHealth() << endl; // garbage value
 
-    // use setter
+
+    // dynamically
+    Hero *b = new Hero;
+    // use setter for health bec it is private
+    b->setHealth(100);
+    b->setLevel('B');
+
+    cout << "level : " << (*b).level << endl;
+    cout << "health is " << (*b).getHealth() << endl;
+
+    cout<<"level is " << b->level << endl;
+    cout<<"health is " << b->getHealth() << endl;
+
+    // // creation of object
+    // Hero ramesh;
+
+    // cout<<"Ramesh health is " << ramesh.getHealth() << endl; // garbage value
+
+    // // use setter
     
-    ramesh.setHealth(70);
-    // ramesh.health = 70;
-    ramesh.level = 'A';
+    // ramesh.setHealth(70);
+    // // ramesh.health = 70;
+    // ramesh.level = 'A';
 
-    cout << "health is : " << ramesh.getHealth() << endl;
-    cout << "level is : " << ramesh.level << endl;
+    // cout << "health is : " << ramesh.getHealth() << endl;
+    // cout << "level is : " << ramesh.level << endl;
 
-    // cout<<"size : "<< sizeof(h1) << endl;
+    // // cout<<"size : "<< sizeof(h1) << endl;
 
     return 0;
 }
