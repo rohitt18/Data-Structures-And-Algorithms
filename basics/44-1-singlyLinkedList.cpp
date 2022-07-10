66fca81ca4ec9e8c94d1e2782a03ba2b437b5ea8
Rohit Gupta <rohitgupta9769@gmail.com>
2022-07-11 01:27:35 +0530

    singly linked list

---
+++ basics/44-1-singlyLinkedList.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// node class
+class Node {
+
+    public:
+    int data;
+    Node* next;
+
+    // constructor
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+
+};
+
+
+int main()
+{
+    Node* node1 = new Node(10);
+
+    cout << node1->data << endl;
+    cout << node1->next << endl;
+
+    return 0;
+}
