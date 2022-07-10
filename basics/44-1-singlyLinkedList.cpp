5a41d36761a6f7ce347004473e205ef052587259
Rohit Gupta <rohitgupta9769@gmail.com>
2022-07-11 02:22:59 +0530

    singly linked list insertion

--- basics/44-1-singlyLinkedList.cpp
+++ basics/44-1-singlyLinkedList.cpp
@@ -16,13 +16,65 @@ class Node {
 
 };
 
+void InsertAtHead(Node* &head, int data){
+     
+    // jabhi koi data aata hai uske liye new node create karlo
+    Node * temp = new Node(data);
+
+    // jo nayi node create ki hai uska pointer jo null ko...
+    // ...point karra hai usko hata ke head wali node ko point karva do
+    temp->next = head;
+
+    // head ko temp pe point karva do
+    head = temp; 
+
+}
+
+void InsertAtTail(Node* &tail, int data){   
+
+    // new node create
+    Node* temp = new Node(data);
+
+    // tail ke next ko temp pe point karva do
+    tail->next = temp;
+
+    // tail ko temp pe point karva do
+    tail = temp;
+}   
+
+void print(Node* &head) {
+
+    Node* temp = head;
+    while(temp != NULL){ 
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }cout<<endl;
+} 
 
 int main()
 {
+    // creating a new node
     Node* node1 = new Node(10);
 
-    cout << node1->data << endl;
-    cout << node1->next << endl;
+    // cout << node1->data << endl;
+    // cout << node1->next << endl;
+
+    // head pointed to node1
+    Node* head = node1;
+    Node* tail = node1;
+    print(head);
+
+    // InsertAtHead(head, 12);
+    // print(head);
+
+    // InsertAtHead(head,15);
+    // print(head);
+
+    InsertAtTail(tail,12);
+    print(head);
+
+    InsertAtTail(tail,15);
+    print(head);
 
     return 0;
 }
