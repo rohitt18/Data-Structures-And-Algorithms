6ab038f2659ac84d269c61a75abdca28d0e84e59
Rohit Gupta <rohitgupta9769@gmail.com>
2022-07-13 19:07:33 +0530

    circular linked list insertion

--- advanced/44-3-circularLinkedList.cpp
+++ advanced/44-3-circularLinkedList.cpp
@@ -25,13 +25,89 @@ class Node {
 
 };
 
-void insertNode(Node* tail, int element, int d) {
-    
+void insertNode(Node* &tail, int element, int d) {
+
+    // empty list
+    if(tail == NULL){
+        Node* newNode = new Node(d);
+        tail = newNode;
+        newNode->next = newNode;
+    }
+    else{
+        // non-empty list
+        // assuming that the element is present in the list
+
+        Node* curr = tail;
+
+        while(curr->data != element){
+            curr = curr->next; // curr ko aage badhate raho
+        }
+        // element found
+        Node* temp = new Node(d);
+        temp->next = curr->next;
+        curr->next = temp;
+    }
+
 }
+
+void print(Node* &tail) {
+
+    Node* temp = tail;
+    
+    // empty list
+    if(tail == NULL){
+        cout<<"list is empty"<<endl;
+        return;
+    }
+    
+    do{
+        cout << tail->data << " "; 
+        tail = tail->next;
+    } while(tail != temp);
+
+    cout<<endl;
+
+}
+
+// void print(Node *tail)
+// {
+
+//     Node *temp = tail;
+
+//     // empty list
+//     if (tail == NULL)
+//     {
+//         cout << "List is Empty " << endl;
+//         return;
+//     }
+
+//     do
+//     {
+//         cout << tail->data << " ";
+//         tail = tail->next;
+//     } while (tail != temp);
+
+//     cout << endl;
+// }
 
 int main()
 {   
-    
+    Node* tail = NULL;
+
+    print(tail);
+
+    // empty list mai insert karre hai
+    insertNode(tail,5,3);
+    print(tail);
+
+    insertNode(tail,3,5);
+    print(tail);
+
+    insertNode(tail,5,7);
+    print(tail);
+
+    insertNode(tail,7,9);
+    print(tail);
 
     return 0;
 }
