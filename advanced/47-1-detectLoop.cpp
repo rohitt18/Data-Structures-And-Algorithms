98254d7cfdd2d25036558cf6385dba57414bf476
Rohit Gupta <rohitgupta9769@gmail.com>
2022-07-17 17:15:27 +0530

    floyd's algo

--- advanced/47-1-detectLoop.cpp
+++ advanced/47-1-detectLoop.cpp
@@ -1,5 +1,6 @@
-bool detectLoop(Node *head)
-{
+// Approach 1 : using hash-map
+
+bool detectLoop(Node *head) {
     if (head == NULL)
     {
         return false;
@@ -19,5 +20,31 @@ bool detectLoop(Node *head)
         temp = temp->next;
     }
 
+    return false;
+}
+
+// Approach 2 : floyd's algo
+
+bool detectLoop(Node *head) {
+    Node *slow = head;
+    Node *fast = head;
+
+    if (head == NULL || head->next == NULL){
+        return false;
+    }
+
+    while (fast != NULL){
+        fast = fast->next;
+        if (fast != NULL)
+        {
+            fast = fast->next;
+        }
+        slow = slow->next;
+
+        if (fast == slow)
+        {
+            return true;
+        }
+    }
     return false;
 }
