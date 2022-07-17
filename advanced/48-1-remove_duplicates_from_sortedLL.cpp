efa9db1b6404efa4493170f63bb35f661d281a91
Rohit Gupta <rohitgupta9769@gmail.com>
2022-07-18 04:06:31 +0530

    remove duplicates from a sorted array

---
+++ advanced/48-1-remove_duplicates_from_sortedLL.cpp
@@ -0,0 +1,26 @@
+Node *uniqueSortedList(Node *head)
+{
+
+    // empty
+    if (head == NULL)
+    {
+        return NULL;
+    }
+
+    // non empty list
+
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        if ((curr->next != NULL) && curr->data == curr->next->data)
+        {
+            delete (curr->next);
+            curr->next = curr->next->next;
+        }
+        else // not equal
+        {
+            curr = curr->next;
+        }
+    }
+    return head;
+}
---
+++ advanced/48-2-remove_duplicates_from_UNsortedLL.cpp
@@ -0,0 +1,62 @@
+// Approach 1 : 2 LOOPS O(n2) O(1)
+
+Node *removeDuplicates(Node *head)
+{
+    if (head == NULL)
+    {
+        return NULL;
+    }
+
+    // non-empty LL => atleast ek node hai
+    Node *curr = head;
+    while (curr != NULL && curr->next != NULL)
+    {
+        Node *prev = curr;
+        while (prev->next != NULL)
+        {
+            if (curr->data == prev->next->data)
+            {
+                //                 Node* tempKaNext = temp->next;
+                Node *duplicate = prev->next;
+                prev->next = prev->next->next;
+                //                 temp->next = NULL;
+                delete (duplicate);
+            }
+            else
+            {
+                prev = prev->next;
+            }
+        }
+        curr = curr->next;
+    }
+    return head;
+}
+
+
+// Approach 2 : using map O(n) O(n)
+
+Node *removeDuplicates(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+        return head;
+    unordered_map<int, int> m;
+    Node *curr = head;
+    m[curr->data] = 1;
+    Node *prev = curr;
+    curr = curr->next;
+    while (curr != NULL)
+    {
+        if (m[curr->data])
+        {
+            prev->next = curr->next;
+            free(curr);
+        }
+        else
+        {
+            m[curr->data] = 1;
+            prev = curr;
+        }
+        curr = prev->next;
+    }
+    return head;
+}
